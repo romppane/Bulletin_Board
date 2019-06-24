f5651cd73ffbc28cea29d4ca97171b567f8f08d6
Roni Metsämuuronen <roni.metsamuuronen@hotmail.com>
2019-06-24 15:54:23 +0300

    fix source for npm run prod

--- build/server.js
+++ build/server.js
@@ -1,11 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// lib/app.ts
 var express = require("express");
+var Student = /** @class */ (function () {
+    function Student(firstname, middleinitial, lastname) {
+        this.firstname = firstname;
+        this.middleinitial = middleinitial;
+        this.lastname = lastname;
+        this.fullname = firstname + " " + middleinitial + " " + lastname;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstname + " " + person.lastname;
+}
 // Create a new express application instance
 var app = express();
+var user = new Student("Tero", "TT", "Tekijä");
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send(user.fullname);
+});
+app.get('/person', function (req, res) {
+    res.send(greeter(user));
+});
+app.post('/person', function (req, res) {
 });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
