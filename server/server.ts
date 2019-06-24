a976c34804cf649c707129eedcac6b28067544cb
Roni Metsämuuronen <roni.metsamuuronen@hotmail.com>
2019-06-24 15:31:30 +0300

    added ts-jest for unit testing, the root directory has been set to /server

--- server/server.ts
+++ server/server.ts
@@ -7,12 +7,31 @@ class Student {
   }
 }
 
+interface Person {
+  firstname: string;
+  lastname: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstname + " " + person.lastname;
+}
+
 // Create a new express application instance
 const app: express.Application = express();
 const user = new Student("Tero", "TT", "Tekijä");
 app.get('/', function (req, res) {
   res.send(user.fullname);
 });
+
+
+
+app.get('/person', (req, res) => {
+  res.send(greeter(user));
+})
+
+app.post('/person', (req,res) => {
+
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
