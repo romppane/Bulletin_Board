76e627caef55a9eb04c674b4c5c31898591bf4a9
Roni Metsämuuronen <roni.metsamuuronen@hotmail.com>
2019-06-25 12:32:38 +0300

    reading dummies and posting a new one are working

--- server/server.ts
+++ server/server.ts
@@ -1,37 +1,81 @@
 import express = require('express');
+import bodyparser = require ('body-parser');
 
-class Student {
-  fullname : string;
-  constructor(public firstname : string, public middleinitial : string, public lastname : string) {
-    this.fullname = firstname + " " + middleinitial + " " + lastname;
+interface Note {
+  id: number;
+  owner: number;
+  likes: number;
+  category: string;
+  views: number;
+  message: string;
+}
+
+class Post implements Note{
+  id: number;
+  owner: number;
+  likes: number;
+  category: string;
+  views: number;
+  message: string;
+
+  constructor(id: number, owner: number, likes: number, category: string, views: number, message: string){
+    this.id = id;
+    this.owner = owner;
+    this.likes = likes;
+    this.category = category;
+    this.views = views;
+    this.message = message;
   }
 }
 
-interface Person {
-  firstname: string;
-  lastname: string;
-}
-
-function greeter(person: Person) {
-  return "Hello, " + person.firstname + " " + person.lastname;
-}
 
 // Create a new express application instance
 const app: express.Application = express();
-const user = new Student("Tero", "TT", "Tekijä");
+app.use(bodyparser.json());
+
+// Empty array of posts
+let posts: Post[] = [];
+
+// Fill posts
+for(var i = 0; i<20; i++) {
+  posts.push(new Post(i, i, i, "category", i, "toimii"));
+}
+
+// Basic message to see that connection works
 app.get('/', function (req, res) {
-  res.send(user.fullname);
+  res.send('Bulletin_Board');
 });
 
-
-
-app.get('/person', (req, res) => {
-  res.send(greeter(user));
+// Return all posts
+app.get('/Post', (req, res) => {
+  res.send(posts);
 })
 
-app.post('/person', (req,res) => {
-
+// Dummy for post/id
+app.get('/Post/:id', (req, res) => {
+  res.send(posts[req.params.id]);
 })
+
+// Create post
+app.post('/Post', (req, res) => {
+  const post : Post = req.body;
+  posts.push(post)
+  console.log(posts[posts.length-1]);
+
+  res.sendStatus(200);
+})
+
+// Delete post
+app.delete('/Post/:id', (req, res) => {
+  console.log(posts[req.params.id]);
+  res.sendStatus(200);
+})
+// Update post
+app.put('/Post/:id', (req, res) => {
+  console.log(posts[req.params.id]);
+  res.sendStatus(200);
+})
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
