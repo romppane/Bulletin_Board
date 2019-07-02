de183efa0b288145cf63ed3f87d9f84938ca28e3
Roni Metsämuuronen <roni.metsamuuronen@hotmail.com>
2019-07-02 13:49:11 +0300

    When validating Post, req.body is now only changed in the situation when validation is successfull.

--- server/helpers/validation.ts
+++ server/helpers/validation.ts
@@ -41,14 +41,15 @@ export const postPUTSchema: ValidationSchema = {
 
 export const validatePost = (req: Request, res: Response, next: NextFunction) => {
     // Remove any extra fields and turn the plain object in to instance of a Post
-    req.body = plainToClass(Post, req.body, { excludeExtraneousValues: true })
+    const newpost = plainToClass(Post, req.body, { excludeExtraneousValues: true })
     // See if the newly made Post is valid
-    validate(req.body).then(errors => { // errors is an array of validation errors
+    validate(newpost).then(errors => { // errors is an array of validation errors
         if (errors.length > 0) {
             console.log("validation failed. errors: ", errors);
             res.send(errors);
         } else {
             console.log("validation succeed");
+            req.body = newpost;
             next();
         }
     });
