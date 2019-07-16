10a9b8dc36df57b3a1943a01d38cc0d7de92213b
Roni Metsämuuronen <roni.metsamuuronen@hotmail.com>
2019-07-16 09:47:26 +0300

    Logger switched to its own class

--- server/middleware/logger.ts
+++ server/middleware/logger.ts
@@ -1,12 +1,15 @@
-import * as morgan from 'morgan';
+import morgan = require('morgan');
 import * as fs from 'fs';
 import * as path from 'path';
 import * as moment from 'moment-timezone';
+import express = require('express');
 
 export class Logger {
   infoLogStream: fs.WriteStream;
   errorLogStream: fs.WriteStream;
-  constructor() {
+  app: express.Application;
+  constructor(app: express.Application) {
+    this.app = app;
     this.infoLogStream = fs.createWriteStream(path.join('.', 'logs', 'info.log'), {
       flags: 'a'
     });
@@ -26,6 +29,34 @@ export class Logger {
     morgan.format(
       'incFormat',
       ':remote-addr [:date] :method [:url] HTTP/:http-version [:user-agent]'
+    );
+
+    // Log incoming requests
+    this.app.use(
+      morgan('incFormat', {
+        immediate: true,
+        stream: this.infoLogStream
+      })
+    );
+
+    // Log successfull responces
+    this.app.use(
+      morgan('outFormat', {
+        skip: (req: express.Request, res: express.Response) => {
+          return res.statusCode >= 399;
+        },
+        stream: this.infoLogStream
+      })
+    );
+
+    // Logs error responces
+    this.app.use(
+      morgan('outFormat', {
+        skip: (req: express.Request, res: express.Response) => {
+          return res.statusCode < 400;
+        },
+        stream: this.errorLogStream
+      })
     );
   }
 }
--- server/server.ts
+++ server/server.ts
@@ -11,15 +11,12 @@ import { PostRouter } from './routes/post-router';
 import { Dependencies } from './types';
 import { UserRouter } from './routes/user-router';
 import { ReplyRouter } from './routes/reply-router';
+import { Logger } from './middleware/logger';
 
 // It's essential to register schemas. Otherwise all will pass.
 registerSchema(postPUTSchema);
 registerSchema(replyPUTSchema);
 registerSchema(requestParamSchema);
-
-// Construct morgan
-const morgan = require('morgan');
-const moment = require('moment-timezone');
 
 export class Server {
   // Create a new express application instance
@@ -27,68 +24,17 @@ export class Server {
   postRouter: PostRouter;
   userRouter: UserRouter;
   replyRouter: ReplyRouter;
+  logger: Logger;
   constructor(options: Dependencies) {
     this.app = express();
     this.postRouter = options.postRouter;
     this.userRouter = options.userRouter;
     this.replyRouter = options.replyRouter;
+    this.logger = new Logger(this.app);
   }
 
   start() {
-    morgan.token('date', (req: Request, res: Response, zone: string) => {
-      return moment()
-        .tz(zone)
-        .format();
-    });
-
-    // :remote-addr - :remote-user , possibly later
-    morgan.format(
-      'outFormat',
-      ':remote-addr [:date[Europe/Helsinki]] :method [:url] HTTP/:http-version [:status] :res[content-length] - :response-time ms [:user-agent]'
-    );
-    morgan.format(
-      'incFormat',
-      ':remote-addr [:date[Europe/Helsinki]] :method [:url] HTTP/:http-version [:user-agent]'
-    );
-
-    const infoLogStream = fs.createWriteStream(path.join('.', 'logs', 'info.log'), {
-      flags: 'a'
-    });
-
-    const errorLogStream = fs.createWriteStream(path.join('.', 'logs', 'error.log'), {
-      flags: 'a'
-    });
-
     this.app.use(bodyparser.json());
-
-    // Log incoming requests
-    this.app.use(
-      morgan('incFormat', {
-        immediate: true,
-        stream: infoLogStream
-      })
-    );
-
-    // Log successfull responces
-    this.app.use(
-      morgan('outFormat', {
-        skip: (req: express.Request, res: express.Response) => {
-          return res.statusCode >= 399;
-        },
-        stream: infoLogStream
-      })
-    );
-
-    // Logs error responces
-    this.app.use(
-      morgan('outFormat', {
-        skip: (req: express.Request, res: express.Response) => {
-          return res.statusCode < 400;
-        },
-        stream: errorLogStream
-      })
-    );
-
     // Routing
     this.app.use('/posts', this.postRouter.router);
     this.app.use('/users', this.userRouter.router);
