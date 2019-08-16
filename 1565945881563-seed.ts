0a96b3a3c3dcb83e1316f7a2363c40b801672da8
Roni Metsämuuronen <roni.metsamuuronen@hotmail.com>
2019-08-16 12:42:03 +0300

    Seed the database on first run

---
+++ 1565945881563-seed.ts
@@ -0,0 +1,9 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class seed1565945881563 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    queryRunner.query("INSERT INTO user (avatar) VALUES ('avatar')");
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {}
+}
--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,10 @@
-import { createConnection } from 'typeorm';
+import { createConnection, getConnection } from 'typeorm';
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
 import { Environment } from './environment';
 import { User } from './entities/user';
 import { Post } from './entities/post';
 import { Comment } from './entities/comment';
+import { seed1565945881563 } from '../1565945881563-seed';
 
 export const connectDB = (Env: Environment) => {
   const db: MysqlConnectionOptions = {
@@ -12,7 +13,14 @@ export const connectDB = (Env: Environment) => {
     database: Env.DB_NAME,
     synchronize: Env.DB_SYNCHRONIZE,
     logging: Env.DB_LOGGING,
-    entities: [User, Post, Comment]
+    entities: [User, Post, Comment],
+    migrations: [seed1565945881563]
   };
-  return createConnection(db);
+  return createConnection(db)
+    .then(connection => connection.runMigrations())
+    .then(() => getConnection())
+    .catch(error => {
+      console.log(error);
+      return getConnection();
+    });
 };
