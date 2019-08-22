40c1d2b77555d54434c3eb3d9a11bf96b452cad3
Roni Metsämuuronen <roni.metsamuuronen@hotmail.com>
2019-08-22 11:38:17 +0300

    Add username migrations back to one file

--- server/migrations/1566461589946-TemporaryUsernames.ts
+++ server/migrations/1566461589946-TemporaryUsernames.ts
@@ -1,11 +1,13 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CommentUsernames1566461597750 implements MigrationInterface {
+export class TemporaryUsernames1566461589946 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query('ALTER TABLE post ADD COLUMN username VARCHAR(18) NOT NULL');
     await queryRunner.query('ALTER TABLE comment ADD COLUMN username VARCHAR(18) NOT NULL');
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query('ALTER TABLE comment DROP COLUMN username');
+    await queryRunner.query('ALTER TABLE post DROP COLUMN username');
   }
 }
