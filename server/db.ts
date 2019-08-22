1be285acc88693fe5b8bddd8bc832c6c7dc3b5ad
Roni Metsämuuronen <roni.metsamuuronen@hotmail.com>
2019-08-22 11:15:37 +0300

    Seperated migrations

--- server/db.ts
+++ server/db.ts
@@ -5,7 +5,8 @@ import { User } from './entities/user';
 import { Post } from './entities/post';
 import { Comment } from './entities/comment';
 import { UserSeed1565945881563 } from './migrations/1565945881563-UserSeed';
-import { TemporaryUsernames1566459567245 } from './migrations/1566459567245-TemporaryUsernames';
+import { PostUsernames1566461589946 } from './migrations/1566461589946-PostUsernames';
+import { CommentUsernames1566461597750 } from './migrations/1566461597750-CommentUsernames';
 
 export const connectDB = (Env: Environment) => {
   const db: MysqlConnectionOptions = {
@@ -15,7 +16,7 @@ export const connectDB = (Env: Environment) => {
     synchronize: Env.DB_SYNCHRONIZE,
     logging: Env.DB_LOGGING,
     entities: [User, Post, Comment],
-    migrations: [UserSeed1565945881563, TemporaryUsernames1566459567245]
+    migrations: [UserSeed1565945881563, PostUsernames1566461589946, CommentUsernames1566461597750]
   };
   return createConnection(db).then(async connection => {
     await connection.runMigrations();
