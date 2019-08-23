948cfae6e356fd66a85830fb3de07110a8f8bd9f
Roni Metsämuuronen <roni.metsamuuronen@hotmail.com>
2019-08-23 11:05:15 +0300

    I guess we doing the stupid solution then

--- server/db.ts
+++ server/db.ts
@@ -5,18 +5,29 @@ import { User } from './entities/user';
 import { Post } from './entities/post';
 import { Comment } from './entities/comment';
 import { UserSeed1565945881563 } from './migrations/1565945881563-UserSeed';
+import { TemporaryUsernames1566461589946 } from './migrations/1566461589946-TemporaryUsernames';
 
 export const connectDB = (Env: Environment) => {
-  const migrations = Env.DB_SYNCHRONIZE ? UserSeed1565945881563 : '/migrations/*';
-  const db: MysqlConnectionOptions = {
-    type: 'mariadb',
-    url: Env.DB_URL,
-    database: Env.DB_NAME,
-    synchronize: Env.DB_SYNCHRONIZE,
-    logging: Env.DB_LOGGING,
-    entities: [User, Post, Comment],
-    migrations: [migrations]
-  };
+  const db: MysqlConnectionOptions = Env.DB_SYNCHRONIZE
+    ? {
+        type: 'mariadb',
+        url: Env.DB_URL,
+        database: Env.DB_NAME,
+        synchronize: Env.DB_SYNCHRONIZE,
+        logging: Env.DB_LOGGING,
+        entities: [User, Post, Comment],
+        migrations: [UserSeed1565945881563]
+      }
+    : {
+        type: 'mariadb',
+        url: Env.DB_URL,
+        database: Env.DB_NAME,
+        synchronize: Env.DB_SYNCHRONIZE,
+        logging: Env.DB_LOGGING,
+        entities: [User, Post, Comment],
+        migrations: [UserSeed1565945881563, TemporaryUsernames1566461589946]
+      };
+
   return createConnection(db).then(async connection => {
     await connection.runMigrations();
     return getConnection();
