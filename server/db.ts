ed846679bf23178c98a2c9bac0bf65e84a7b453b
Roni Metsämuuronen <roni.metsamuuronen@hotmail.com>
2019-08-23 09:18:53 +0300

    migration selector

--- server/db.ts
+++ server/db.ts
@@ -5,9 +5,9 @@ import { User } from './entities/user';
 import { Post } from './entities/post';
 import { Comment } from './entities/comment';
 import { UserSeed1565945881563 } from './migrations/1565945881563-UserSeed';
-import { TemporaryUsernames1566461589946 } from './migrations/1566461589946-TemporaryUsernames';
 
 export const connectDB = (Env: Environment) => {
+  const migrations = process.env.production ? './migrations/*' : UserSeed1565945881563;
   const db: MysqlConnectionOptions = {
     type: 'mariadb',
     url: Env.DB_URL,
@@ -15,7 +15,7 @@ export const connectDB = (Env: Environment) => {
     synchronize: Env.DB_SYNCHRONIZE,
     logging: Env.DB_LOGGING,
     entities: [User, Post, Comment],
-    migrations: [UserSeed1565945881563, TemporaryUsernames1566461589946]
+    migrations: [migrations]
   };
   return createConnection(db).then(async connection => {
     await connection.runMigrations();
